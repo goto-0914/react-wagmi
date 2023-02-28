9830ea51a2401bc868596d8aafb9591889dcfd81
goto.r <goto.r@impl.co.jp>
2023-02-28 16:14:34 +0900

    wrapped with wagmiconfig

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { WagmiConfig } from 'wagmi';
+import { wagmiClient } from './lib/wagmi';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
